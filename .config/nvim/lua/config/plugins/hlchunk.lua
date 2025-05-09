2d3b39b85091c4b5bc3aa5b37559bebec110f914
suzutsuki0114 <209620833+suzutsuki0114@users.noreply.github.com>
2025-05-10 00:35:41 +0900

    hlchunkカスタマイズ
    サクサクチャンクが動く

--- .config/nvim/lua/config/plugins/hlchunk.lua
+++ .config/nvim/lua/config/plugins/hlchunk.lua
@@ -1,24 +1,27 @@
 require('hlchunk').setup({
-  chunk = {
-    enable = true,
-    style = "#c678dd",
-    -- style = "#56b6c2",
-  },
-  indent = {
-    enable = true,
-  },
-  blank = {
-    enable = false, -- なんか動かない
-    chars = {
-      "․",
+    chunk = {
+        enable = true,
+        -- style = "#c678dd",
+        style = "#56b6c2",
+        -- style = "#61afef",
+        duration = "0",
+        delay = "0",
     },
-    style = {
-      { vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("Whitespace")), "fg", "gui"), "" },
+    indent = {
+        enable = true,
     },
-  },
-  line_num = {
-    enable = false,
-    style = "#c678dd",
-  },
+    blank = {
+        enable = true, -- なんか動かない
+    --     chars = {
+    --         "․",
+    --     },
+    --     style = {
+    --         { vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("Whitespace")), "fg", "gui"), "" },
+    --     },
+    },
+    line_num = {
+        enable = false,
+        style = "#c678dd",
+    },
 })
 
