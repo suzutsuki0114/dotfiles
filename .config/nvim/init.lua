8dbfe4ad0289147cb54a0725a1dd9aeed305787f
suzutsuki0114 <209620833+suzutsuki0114@users.noreply.github.com>
2025-05-10 22:02:48 +0900

    init.lua見やすく
    シンプルイズベスト

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,27 +4,6 @@ require('config.vim')
 -- プラグイン設定
 require('config.lazy')
 
--- キーマップ設定
 require('config.keymap')
 vim.opt.encoding = 'utf-8'
 vim.scriptencoding = 'utf-8'
-
--- プラグインの設定とか
--- require('config.plugins.onedarkpro')
--- require('config.plugins.lualine')
--- require('config.plugins.bufferline')
--- require('config.plugins.copilot')
--- require('config.plugins.mason')
--- require('config.plugins.nvim-cmp')
--- require('config.plugins.nvim-treesitter-context')
--- require('config.plugins.nvim-ts-autotag')
--- require('config.plugins.nvim-scrollbar')
--- require('config.plugins.telescope-fzf-native')
--- require('config.plugins.toggleterm')
--- require('config.plugins.accelerated-jk')
--- require('config.plugins.tiny-inline-diagnostic')
--- require('config.plugins.hlchunk')
--- require('config.plugins.gitsigns')
--- require('config.plugins.Comment')
--- require('config.plugins.colorizer')
--- vim.cmd('LspStart')
