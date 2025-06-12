fb5774b21169887342b1333456a44101426d7ff4
suzutsuki0114 <209620833+suzutsuki0114@users.noreply.github.com>
2025-06-12 23:56:05 +0900

    bufferline強化
    見やすくなったねー

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -8,15 +8,33 @@ return {
             require("bufferline").setup{
                 options = {
                     mode = "tabs",
+                    -- separator_style = "slope",
+                    separator_style = "thin",
+                    numbers = "buffer_id",
                     close_command = "tabclose! %d",
                     right_mouse_command = "tab %d",
                     left_mouse_command = "tab %d",
                     middle_mouse_command = "tabclose %d",
+                    hover = {
+                        enabled = true,
+                        delay = 0,
+                        reveal = {'close'}
+                    },
                     diagnostics = "nvim_lsp",
                     diagnostics_indicator = function(count, level, diagnostics_dict, context)
                         local icon = level:match("error") and " " or " "
                         return " " .. icon .. count
                     end,
+
+                    -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
+                    --     local s = " "
+                    --     for e, n in pairs(diagnostics_dict) do
+                    --         local sym = e == "error" and " "
+                    --         or (e == "warning" and " " or " ")
+                    --         s = s .. n .. sym
+                    --     end
+                    --     return s
+                    -- end,
 
                     offsets = {
                         {
