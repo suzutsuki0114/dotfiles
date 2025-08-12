e29ded5fbc97cb3bb354642a14c38d7c09785f19
suzutsuki0114 <209620833+suzutsuki0114@users.noreply.github.com>
2025-08-13 00:32:58 +0900

    カラースキームいじった
    forkすることでプラグインを直接いじれることが判明

--- .config/nvim/lua/plugins/onedarkpro.lua
+++ .config/nvim/lua/plugins/onedarkpro.lua
@@ -1,5 +1,6 @@
 return {
-    "olimorris/onedarkpro.nvim",
+    -- "olimorris/onedarkpro.nvim",
+    "suzutsuki0114/onedarkpro.nvim",
     lazy = false,
     priority = 1000, -- Ensure it loads first
     config = function ()
