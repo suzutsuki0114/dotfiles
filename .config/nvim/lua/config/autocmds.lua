11612580dd105266e1c08f6a751ef5063b6d7441
suzutsuki0114 <209620833+suzutsuki0114@users.noreply.github.com>
2026-01-01 17:49:09 +0900

    :qするときに邪魔なやつも同時に消す

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -38,3 +38,26 @@ function! s:GetBufByte()
 endfunction
 autocmd VimEnter * nested if @% != '' && s:GetBufByte() != 0 | e %:p | endif
 ]])
+
+vim.api.nvim_create_autocmd('QuitPre', {
+  callback = function()
+    -- 現在のウィンドウ番号を取得
+    local current_win = vim.api.nvim_get_current_win()
+    -- すべてのウィンドウをループして調べる
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      -- カレント以外を調査
+      if win ~= current_win then
+        local buf = vim.api.nvim_win_get_buf(win)
+        -- buftypeが空文字（通常のバッファ）があればループ終了
+        if vim.bo[buf].buftype == '' then
+          return
+        end
+      end
+    end
+    -- ここまで来たらカレント以外がすべて特殊ウィンドウということなので
+    -- カレント以外をすべて閉じる
+    vim.cmd.only({ bang = true })
+    -- この後、ウィンドウ1つの状態でquitが実行されるので、Vimが終了する
+  end,
+  desc = 'Close all special buffers and quit Neovim',
+})
